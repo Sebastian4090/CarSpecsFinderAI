6d275034e109f2e9e150e967f52b74377d362df2
Sebastian4090 <bastek4090@gmail.com>
2024-03-05 14:26:43 +0100

    update dropdowns + testing dropdown component

--- src/Components/Dropdown/Dropdown.tsx
+++ src/Components/Dropdown/Dropdown.tsx
@@ -1,35 +1,13 @@
 
-const Dropdown = () => {
+const Dropdown = (data : string[]) => {
+    console.log(data);
+    console.log(data[0]);
     return (
-        <>
         <div className="absolute mt-[2px] max-h-64 overflow-y-scroll shadow-md border-gray-200 border-[1px] rounded-md" id="dropdown">
-            <div className="h-12 w-44 p-2  border-gray-200 bg-white text-xl 
-            hover:bg-blue-500   cursor-pointer">BMW</div>
-            <div className="h-12 w-44 p-2  border-gray-200 bg-white text-xl 
-            hover:bg-blue-500   cursor-pointer">Mercedes</div>
-            <div className="h-12 w-44 p-2  border-gray-200 bg-white text-xl 
-            hover:bg-blue-500   cursor-pointer">Audi</div>
-            <div className="h-12 w-44 p-2  border-gray-200 bg-white text-xl 
-            hover:bg-blue-500   cursor-pointer">Lamborghini</div>
-            <div className="h-12 w-44 p-2  border-gray-200 bg-white text-xl 
-            hover:bg-blue-500   cursor-pointer">Pagani</div>
-            <div className="h-12 w-44 p-2  border-gray-200 bg-white text-xl 
-            hover:bg-blue-500   cursor-pointer">Alfa Romeo</div>
-            <div className="h-12 w-44 p-2  border-gray-200 bg-white text-xl 
-            hover:bg-blue-500   cursor-pointer">Infiniti</div>
-            <div className="h-12 w-44 p-2  border-gray-200 bg-white text-xl 
-            hover:bg-blue-500   cursor-pointer">Chevrolet</div>
-            <div className="h-12 w-44 p-2  border-gray-200 bg-white text-xl 
-            hover:bg-blue-500   cursor-pointer">Ford</div>
-            <div className="h-12 w-44 p-2  border-gray-200 bg-white text-xl 
-            hover:bg-blue-500   cursor-pointer">Dodge</div>
-            <div className="h-12 w-44 p-2  border-gray-200 bg-white text-xl 
-            hover:bg-blue-500   cursor-pointer">Smart</div>
-            <div className="h-12 w-44 p-2  border-gray-200 bg-white text-xl 
-            hover:bg-blue-500   cursor-pointer">Porsche</div>
+        {data.map((element : string) => {
+            return <div key={element} className="h-12 w-44 p-2  border-gray-200 bg-white text-xl hover:bg-blue-500 cursor-pointer">{element}</div>   
+        })}
         </div>
-        </>
-
     )   
 }
 
