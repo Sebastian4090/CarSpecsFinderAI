9d940a609ed60a1a27662025465afd379cd33d84
Sebastian4090 <bastek4090@gmail.com>
2024-03-18 14:30:05 +0100

    Dropdown is now updating state when clicked

--- src/Container/App.tsx
+++ src/Container/App.tsx
@@ -3,25 +3,13 @@ import Label from "../Components/Label/Label";
 import Form from "../Components/Form/Form";
 import Footer from "../Components/Footer/Footer";
 
-interface CarsGenInfo {
-  Brands : object[]
-}
-
-let mockDB: CarsGenInfo = {
-  Brands : {"Audi" : {
-    "A3" : {
-      "gen_8l" : ["1.6"]
-    }
-  }}
-}
-
 function App() {
 
   return (
     <div className="bg-slate-100 h-full">
       <Banner />
       <Label />
-      <Form cars={mockDB}/>
+      <Form />
       <Footer />
     </div>
   )
